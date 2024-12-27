94f77f165b88ea0e3b0bb71e6509a92627605273
mdpolatt <serhatyalcin212121@gmail.com>
2024-12-27 18:03:42 +0300

    hashroter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 
 import Navbar from './components/Navbar'
