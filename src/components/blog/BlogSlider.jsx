be81ddb3018c9e1b1bd1c31523e82a27fee3b041
mdpolatt <serhatyalcin212121@gmail.com>
2024-12-27 15:01:06 +0300

    make it better

--- src/components/blog/BlogSlider.jsx
+++ src/components/blog/BlogSlider.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // CSS dosyasını unutmayın
-import { Link } from "react-router-dom";
+
 import { useAllBlogQuery } from "../../store/apis/blogApi";
 
 const BlogSlider = () => {
